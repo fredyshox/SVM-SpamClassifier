cfbb94d83c4ad00251b0cc21af23bb2650dc87c9
Kacper Raczy <gfw.kra@gmail.com>
2017-10-09 23:35:05 +0200

    Added use of porter stemmer for processing email words.

--- email_processor.py
+++ email_processor.py
@@ -1,14 +1,47 @@
 import numpy as np
 import re
+from stemming.porter2 import stem
 
-class EmailProcessor():
-    @staticmethod
-    def process_email(contents):
-        contents = EmailProcessor.replace_contents(contents)
 
-    @staticmethod
-    def replace_contents(contents):
-        contents = "test_contents"
+class EmailProcessor(object):
+    def __init__(self, word_arr):
+        """
+        Initialize EmailProcessor object with array or words.
+        :param word_arr: array of available words
+        """
+        super.__init__()
+        self.word_arr = word_arr
+
+
+    def process_email(self, contents):
+        contents = "test_contents"#Test
+        contents = self.replace_contents(contents)
+
+        # Get all the words by ommiting all punctuaction
+        pattern_str = ' @$/#.-:&*+=[]?!(){},''">_<;%' + chr(10) + chr(13)
+        words = re.split(pattern_str, contents)
+        word_indexes = []
+
+        for word in words:
+            # Remove all non-alphanumeric chars
+            word = re.sub('[\W]', '', word)
+            word = re.sub('[_]', '', word)
+
+            # Porter stemming word
+            stem_word = stem(word)
+
+            # check if word exist in word_dict
+            # if so, append it's index to word_indexes
+            if word in self.word_arr:
+                index = self.word_arr.index(word)
+                word_indexes.append(index)
+            #TODO print result email
+
+        return word_indexes
+
+
+    def replace_contents(self, contents):
+        contents = "test_contents"#Test
         has_header = True
 
         # If this is raw email, find email header and remove ( \n\n )
